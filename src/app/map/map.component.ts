6f2617f4aeac254b9fd3bdb16ffa32294de8039d
rielyalkov <58986485+rielyalkov@users.noreply.github.com>
2020-09-22 22:05:14 +0300

    new commit

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -23,10 +23,10 @@ export class MapComponent implements OnInit {
   };
 
   ngOnInit(): void {
-    const myMap = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(myMap);
+    // const myMap = L.map('map').setView([51.505, -0.09], 13);
+    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //   attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(myMap);
   }
 
 }
