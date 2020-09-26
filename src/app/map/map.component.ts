26a9825b9851be8b5afc0da190543c07762887d8
rielyalkov <58986485+rielyalkov@users.noreply.github.com>
2020-09-26 16:36:17 +0300

    создание двух слоев для карты, добавление меток по походам

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,7 @@
 import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
 import {latLng, tileLayer, Layer} from 'leaflet';
 import * as L from 'leaflet';
+import {MatRadioButton, MatRadioChange} from '@angular/material/radio';
 
 @Component({
   selector: 'app-map',
@@ -14,6 +15,12 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit{
   ) {
   }
 
+  favoriteSeason = 'Географическая карта';
+  seasons: string[] = ['Географическая карта', 'Топографическая карта'];
+
+  OpenTopoMap = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png';
+  OpenStreetMap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
 
   ngOnInit(): void {
     document.getElementById('mapHTML').innerHTML = "<div id='map' style='height: 500px;' leaflet></div>"
@@ -23,15 +30,40 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit{
       shadowSize:   [0, 26],
       iconAnchor:   [12, 24],
       shadowAnchor: [4, 62],
-      popupAnchor:  [0, -24]
+      popupAnchor:  [0, -26]
     });
 
     this.Map = L.map('map').setView([60.000, 100.000], 3);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    this.mapStyleDefine(this.OpenStreetMap);
+    const markerKh = L.marker([67.734720, 33.726110], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Хибины</b>');
+    L.marker([55.259720, 59.792500], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Таганай</b>');
+    L.marker([43.345830, 42.448610], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Эльбрус</b>');
+    L.marker([56.949400, 32.838600], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Верхневолжские озера</b>');
+    L.marker([56.427395, 28.820091], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Великая</b>');
+    L.marker([67.500000, 66.000000], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Полярный Урал</b>');
+    L.marker([64.838539, 33.693727], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Карелия</b>');
+    L.marker([44.263300, 40.171900], {icon: lIcon}).addTo(this.Map).bindPopup('<b>Адыгея</b>');
+  }
+
+  change(event: MatRadioChange): void {
+    console.log(event);
+    switch (event.value) {
+      case ('Топографическая карта'):
+        console.log('1');
+        this.mapStyleDefine(this.OpenTopoMap);
+        break;
+      case ('Географическая карта'):
+        console.log('2');
+        this.mapStyleDefine(this.OpenStreetMap);
+        break;
+    }
+  }
+
+  mapStyleDefine(mapStyle): void {
+    L.tileLayer(mapStyle, {
+      maxZoom: 19,
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
     }).addTo(this.Map);
-    const marker = L.marker([67.44, 33.43 ], {icon: lIcon}).addTo(this.Map);
-    marker.bindPopup('<b>Хибины</b>');
   }
 
   ngAfterViewInit(): void {
@@ -39,6 +71,5 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit{
 
   ngOnDestroy(): void {
   }
-
 
 }
