cd1bc5e560d16078bd6febd899de700543abb803
rielyalkov <58986485+rielyalkov@users.noreply.github.com>
2020-09-26 12:57:01 +0300

    исправлен баг с загрузкой карты, добавлен попап при нажатии на маркер на карте, работа со стилями маркеров

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -14,23 +14,30 @@ export class MapComponent implements OnInit, OnDestroy, AfterViewInit{
   ) {
   }
 
+
   ngOnInit(): void {
+    document.getElementById('mapHTML').innerHTML = "<div id='map' style='height: 500px;' leaflet></div>"
     const lIcon = L.icon({
-      iconUrl: 'assets/images/location.svg'
+      iconUrl: 'assets/images/location.svg',
+      iconSize:     [24, 24],
+      shadowSize:   [0, 26],
+      iconAnchor:   [12, 24],
+      shadowAnchor: [4, 62],
+      popupAnchor:  [0, -24]
     });
+
     this.Map = L.map('map').setView([60.000, 100.000], 3);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(this.Map);
     const marker = L.marker([67.44, 33.43 ], {icon: lIcon}).addTo(this.Map);
+    marker.bindPopup('<b>Хибины</b>');
   }
 
   ngAfterViewInit(): void {
-    this.Map = undefined;
   }
 
   ngOnDestroy(): void {
-    this.Map = undefined;
   }
 
 
