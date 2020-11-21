410515a64fa9fa462fe5426b7a7df90d9c4f9c2d
rielyalkov <58986485+rielyalkov@users.noreply.github.com>
2020-11-21 16:23:07 +0300

    template for main page

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {MatIconRegistry} from '@angular/material/icon';
+import {DomSanitizer} from '@angular/platform-browser';
+import {Router} from '@angular/router';
+import {MapService} from '../pages/map/mapService/map-service.service';
 
 @Component({
   selector: 'app-main-page',
@@ -8,11 +11,23 @@ import { AngularFirestore } from '@angular/fire/firestore';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() {
-
+  constructor(iconRegistry: MatIconRegistry,
+              sanitizer: DomSanitizer,
+              private router: Router,
+              private service: MapService
+  ) {
   }
 
   ngOnInit(): void {
+  }
+
+  navigateToAbout(): void {
+    this.router.navigateByUrl('/about');
+    this.service.makeArray();
+  }
+
+  navigateToMain(): void {
+    this.router.navigateByUrl('');
   }
 
 }
