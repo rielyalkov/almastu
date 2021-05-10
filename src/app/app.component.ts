3e8b5ec7eea07951b9bccc254cae09826daaa05c
Andrew <bxkr@mail.com>
2021-05-10 12:06:55 +0300

    Set up authorization mechanism + Add a form tag to remember passwords in browsers + Fix root selector + Fix some fire problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
 import {environment} from '../environments/environment.prod';
 
 firebase.initializeApp(environment.firebase);
 
 @Component({
-  selector: 'root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +11,30 @@ import {Component} from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor() { }
+  constructor(
+    private snackBar: MatSnackBar,
+    private route: Router
+  ) { }
   hide = true;
+
+  signIn(usernameRaw: string, password: string): void {
+    const username = usernameRaw + '@almastu-dmd.web.app';
+    firebase.auth().signInWithEmailAndPassword(username, password).then(() => {
+      this.route.navigate(['/']).then();
+    })
+      .catch((error) => {
+        const errorCode = error.code;
+        let message = 'неизвестная проблема';
+        if (errorCode === 'auth/user-not-found') {
+          message = 'пользователь не найден';
+        } else if (errorCode === 'auth/wrong-password' && password !== '') {
+          message = 'неправильный пароль';
+        } else if (errorCode === 'auth/wrong-password' && password === '') {
+          message = 'введите пароль в поле!';
+        } else if (errorCode === 'auth/invalid-email') {
+          message = 'введите имя пользователя в поле!'; }
+        this.snackBar.open('Ошибка: ' + message, null, {duration: 5000});
+      });
+  }
 
 }
