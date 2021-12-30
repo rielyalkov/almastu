61ee4e6636b6bd030a36a49ec07e2e4768f8b6be
Andrew <bxkr@mail.com>
2021-12-30 18:16:36 +0300

    format functionality

--- src/app/panel/news-editor/news-editor.component.ts
+++ src/app/panel/news-editor/news-editor.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Inject, OnInit, ViewChild } from '@angular/core';
 import { MAT_BOTTOM_SHEET_DATA, MatBottomSheet, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
@@ -12,8 +12,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class NewsEditorComponent implements OnInit {
 
-  @ViewChild('text') text;
-  selection = null;
+  @ViewChild('text') text: ElementRef;
+  selection: [number, number, string] = [0, 0, ''];
 
   onRightClick(event: MouseEvent): void {
     event.preventDefault();
@@ -24,19 +24,30 @@ export class NewsEditorComponent implements OnInit {
     const sheet = this.bottomSheet.open(EditorSheetComponent, {data: this.selection});
     sheet.afterDismissed().subscribe((obj) => {
       if (obj) {
-        const dialog = obj.dialog as MatDialogRef<EditorInsertDialogComponent>;
-        dialog.afterClosed().subscribe((objDial) => {
-          if (objDial) {
-            const textToAdd = objDial.data;
-            this.text.nativeElement.value += textToAdd;
-          }
-        });
+        if (obj.dialog) {
+          const dialog = obj.dialog as MatDialogRef<EditorInsertDialogComponent>;
+          dialog.afterClosed().subscribe((objDial) => {
+            if (objDial) {
+              const textToAdd = objDial.data;
+              this.text.nativeElement.value += textToAdd;
+            }
+          });
+        }
+        if (obj.formattedStr) {
+          const txt = obj.formattedStr[0];
+          const indices = obj.formattedStr[1];
+          let current = (this.text.nativeElement.value as string).split('');
+          const currentEnd = [...current.slice(indices[1])];
+          current.splice(indices[0], (indices[1] - indices[0]) + currentEnd.length);
+          current = [...current, ...txt.split(''), ...currentEnd];
+          this.text.nativeElement.value = current.join('');
+        }
       }
     });
   }
 
   recordSelection(): void {
-    this.selection = window.getSelection();
+    this.selection = [this.text.nativeElement.selectionStart, this.text.nativeElement.selectionEnd, window.getSelection().toString()];
   }
 
   send(title, text): void {
@@ -62,28 +73,35 @@ export class NewsEditorComponent implements OnInit {
 @Component({
   selector: 'app-editor-sheet',
   templateUrl: 'templates/bottom-sheet.html',
+  styleUrls: ['templates/bottom-sheet.css']
 })
-export class EditorSheetComponent implements OnInit {
+export class EditorSheetComponent {
 
   constructor(
-    @Inject(MAT_BOTTOM_SHEET_DATA) public selection: string,
+    @Inject(MAT_BOTTOM_SHEET_DATA) public selectionShared: [number, number, string],
     private bs: MatBottomSheetRef,
     public dialog: MatDialog
-  ) {
-  }
+  ) {}
 
-  ngOnInit(): void {
-    if (this.selection.toString() !== '') {
-      document.getElementById('formatting').style.display = 'block';
-      document.getElementById('selection').innerHTML = this.selection;
-    } else {
-      document.getElementById('add').style.display = 'block';
-    }
-  }
+  offsets: [number, number] = [this.selectionShared[0], this.selectionShared[1]];
+  flags: string[] = [];
+  selectionText = this.selectionShared[2];
 
   onTapped(event, choice): void {
     const dialog = this.dialog.open(EditorInsertDialogComponent, {data: choice});
     this.bs.dismiss({dialog});
+  }
+
+  preview(): string {
+    let result = this.selectionText;
+    this.flags.forEach((tag) => {
+      result = `<${tag}>` + result + `</${tag}>`;
+    });
+    return result;
+  }
+
+  formatDone(): void {
+    this.bs.dismiss({formattedStr: [this.preview(), this.offsets]});
   }
 
 }
