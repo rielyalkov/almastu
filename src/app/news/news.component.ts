39a4855057c47cd84af6ce42e1e004e09d37d284
Andrew <bxkr@mail.com>
2021-12-31 15:36:05 +0300

    newsfeed improvements

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -3,16 +3,26 @@ import { Observable } from 'rxjs';
 import { fd, News, NewsService } from './service/news.service';
 import { tap } from 'rxjs/operators';
 import { getAuth } from '@angular/fire/auth';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  styleUrls: ['./news.component.css'],
+  animations: [
+    trigger('fadeOut', [
+      transition(':leave', animate(
+        1000,
+        keyframes([style({opacity: 1, easing: 'ease', offset: 0}), style({opacity: 0, easing: 'ease', offset: 1})])
+      ))
+    ])
+  ]
 })
 export class NewsComponent implements OnInit {
 
   news: Observable<News[]>;
-  isEmpty = true;
+  isEmpty = false;
+  loaded = false;
   authorized: any;
 
   fd(date: any): string {
@@ -22,12 +32,15 @@ export class NewsComponent implements OnInit {
   constructor(private newsService: NewsService) {
   }
 
-  ngOnInit(): void {
+  getNews(): void {
     this.news = this.newsService.getCollection().pipe(tap((q) => {
-      if (q.length !== 0) {
-        this.isEmpty = false;
-      }
+      this.loaded = true;
+      this.isEmpty = q.length === 0;
     }));
+  }
+
+  ngOnInit(): void {
+    this.getNews();
     getAuth().onAuthStateChanged((user) => {
       this.authorized = user;
     });
