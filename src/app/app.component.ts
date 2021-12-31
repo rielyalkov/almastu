fa6b9adcae44e955616393030c9526c4450002ab
Andrew <bxkr@mail.com>
2021-12-31 11:23:40 +0300

    snow :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  snowflakesRange = Array(200).fill(0);
 
   constructor() {
   }
